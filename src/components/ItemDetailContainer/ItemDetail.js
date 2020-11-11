1e50821a6af5128809c3b7ac27497ccc9a20318d
FlorLapetina <florencia.lapetina@bethedriver.global>
2020-11-11 16:53:13 -0300

    commit

--- src/components/ItemDetailContainer/ItemDetail.js
+++ src/components/ItemDetailContainer/ItemDetail.js
@@ -3,16 +3,11 @@ import ItemCount from '../ItemCount/ItemCount';
 import SizeSelector from './SizeSelector';
 import Button from 'react-bootstrap/Button';
 
-
 function ItemDetail({ item }){
-
     const [cart, setCart] = useState(0);
     const [qState, setQState] = useState(null);
     const [count, setCount] = useState(null);
-    const [button, setButton] = useState(null);
-
-    setButton(false); 
-
+    const [button, setButton] = useState(false);
     const handleAdd = (quantity, stock)=>{
         if (quantity <= stock) {
           return()=>{
@@ -23,8 +18,7 @@ function ItemDetail({ item }){
           }	
         }      
     }
-
-    return  <>
+    return  (<>
             <div className="row">
                 <div className="col-6">
                     <img src={item.urlImg} className="w-100"></img>
@@ -35,10 +29,10 @@ function ItemDetail({ item }){
                     <h5>Precio: $<span>{item.price}</span></h5>
                     <SizeSelector />
                     <ItemCount count={count} stock={20} initial={1} onAdd={handleAdd} />
-                    <Button variant="primary" block button={button}><Link to="/cart">Termina tu compra ({qState})</Link></Button>
+                    {/*<Button variant="primary" block button={button}><Link to="/cart">Termina tu compra ({qState})</Link></Button>*/}
                 </div> 
             </div>
-    </>
+    </>)
 }
 
 export default ItemDetail;
